3f0d1e332fc3bb7452d4c07b259f3901fccc2c7e
Tales Lopes <tales.lopes@mercadolivre.com>
2022-11-29 11:35:34 -0300

    feature/new-scope-vs-stg

--- main.go
+++ main.go
@@ -21,6 +21,8 @@ const (
 	OutputFilePath = "output"
 	BlockSize      = 30
 	RoutinesAmount = 6
+	FuryToken      = "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6ImQ0ZDlhMGQzLWM4YTItNDY0Yi1hMGE5LWU3MWM2OTA0MjExNiIsInR5cCI6IkpXVCJ9.eyJhZGRpdGlvbmFsX2luZm8iOnsiZW1haWwiOiJ0YWxlcy5sb3Blc0BtZXJjYWRvbGl2cmUuY29tIiwiZnVsbF9uYW1lIjoiVGFsZXMgQmFsdGFyIExvcGVzIERhIFNpbHZhIiwidXNlcm5hbWUiOiJ0YWxvcGVzIn0sImV4cCI6MTY2OTc1NzYwNCwiaWF0IjoxNjY5NzI1MjA0LCJpZGVudGl0eSI6Im1ybjpzZWdpbmY6YWQ6dXNlci90YWxvcGVzIiwiaXNzIjoiZnVyeV90aWdlciIsInN1YiI6InRhbG9wZXMifQ.fDVTJJLPJHsJU0LEXAV7a7QiJx9TWXBSbllB-dF4Vqx1F1KlM-BgCAUH0VwyycavfRlqCrH43yFk_9I3r5LaPWbW6czNofsdwpLjH_G4R-c-b5bJqytkuIxNLViuByNJrWirXCS5gjepQg6kAmuNIWhgC4o4M-CoicJT7MljOFkGz9od4bijryqoMNWC_6yilk8F_sPlJIfP5Ien8TXbEcyhPsxV_H2jWwIaHqaJRrd4S3De64PSFu_3k8MelNwqsuSa0pIR_WZrTT7WA_4_quCImF3nLt4NIGGGKLf-SdFekFqz4HG5xrKNJiEpmFxAyQtmnFkmi6SszdL42J4Txw"
+	RequestHeader  = "X-Tiger-Token"
 )
 
 var OutputHeader = []string{"arn", "reconciliation_date", "settlement_date", "value_date", "merchant_date", "working_days", "calendar_days", "valid_to_utc"}
@@ -43,6 +45,8 @@ func (r Response) converser(arn string) []string {
 
 func getData(data [][]string, begin int, end int, i int) {
 
+	client := http.Client{}
+
 	var response Response
 
 	mappedResponses := make(map[string][][]string)
@@ -53,9 +57,13 @@ func getData(data [][]string, begin int, end int, i int) {
 		currentArn := data[i][0]
 
 		// Using net/http for a HTTP GET request
-		resp, err := http.Get(currentURL)
+		// resp, err := http.Get(currentURL)
+		req, err := http.NewRequest("GET", currentURL, nil)
 
 		check(err)
+
+		req.Header.Set(RequestHeader, FuryToken)
+		resp, err := client.Do(req)
 
 		if err == nil {
 			body, err := ioutil.ReadAll(resp.Body)
